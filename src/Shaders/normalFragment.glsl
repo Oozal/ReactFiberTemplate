e296f4fa124fe0c8c4b16acbcc56d2ad158fe9ed
Ujjawal <ujjawalpra@gmail.com>
2025-03-15 17:05:20 +0545

    include file containing simplex noise added

--- src/Shaders/normalFragment.glsl
+++ src/Shaders/normalFragment.glsl
@@ -1,8 +1,12 @@
 varying vec2 vUv;
 uniform float uTime;
 
+#include ./includes/noises.glsl
+
 void main() {
 
-    vec3  color = vec3(vUv.xy,0);
+    vec2 newUv = vUv*5.;
+    newUv.x+=uTime;
+    vec3  color = vec3(noise(newUv));
     gl_FragColor = vec4(color, 1);
 }
